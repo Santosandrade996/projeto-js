e73d5198bcfe38fe741fdaa80c683e7a756afdc4
giselle-dio <giselleandrade1996@gmail.com>
2025-05-17 11:28:20 -0300

    Aulão

---
+++ aulas/aula.js
@@ -0,0 +1,12 @@
+//nome, idade, numero de telefone, cpf, endereço, recebe pensão
+//string-armazenar texto
+// number - armazenar numeros
+//boolean- armazenar valores lógicos
+let nomesPokemon =[ "Pikachu", "chamander", "Bulbassaur"]
+let timePokemon = [
+    ["Pikachu", "M", "level1"],
+    ["chamander", "F", "level 7"]
+   
+]
+console.log(timePokemon.length) //chamander
+
