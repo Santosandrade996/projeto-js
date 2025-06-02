1f8e82b12fed3fd81bf6a9124c4ce480f5e183ee
giselle-dio <giselleandrade1996@gmail.com>
2025-06-02 13:24:25 -0300

    djkhjhf

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("Hello, Pokedex!");
+const offset = 0
+const limit = 10
+const pokemonList = document.getElementById("pokemon-list");
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url).then(function(response) {
+    console.log(response);
+})
+
+const x = 10 + 10;
+console.log(x);
