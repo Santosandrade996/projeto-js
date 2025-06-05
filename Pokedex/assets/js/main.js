3accc1d5163d79f2826b3b6213f5530f3839edcc
giselle-dio <giselleandrade1996@gmail.com>
2025-06-05 13:21:56 -0300

    atualizando mais uma aula

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,9 +1,36 @@
 const offset = 0
 const limit = 10
-const pokemonList = document.getElementById("pokemon-list");
+//const pokemonList = document.getElementById("pokemon-list");
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToLi(pokemon) {
+     return ` 
+          <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name"> ${pokemon.name} </span>
+                
+                <div class="details">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+
+                    </ol>
+                    <img src="/Pokedex/assets/imagens/Bulbasaur.png" alt="${pokemon.name}">
+                </div>
+                
+          </li>`;
+}
+
+const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)
      .then(response => response.json())
-    .then(jsonBody => console.log(jsonBody))
-    .catch(error => console.error(error))
+     .then(jsonBody => jsonBody.results)
+     .then(pokemons => {
+     
+          for (let i = 0; i < pokemons.length; i++) {
+               const pokemon = pokemons[i];
+               pokemonList.innerHTML += convertPokemonToLi(pokemon);
+          }
+     })
+     .catch(error => console.error(error));
